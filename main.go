254d3e5e17c9989a10cb0827d5ec52eee582e1cc
bth <gh@hautogdoad.com>
2024-10-07 22:52:15 -0700

    type Set map[string]bool
    and use for tagmap

--- main.go
+++ main.go
@@ -21,11 +21,8 @@ type Entry struct {
 	tags     []string
 }
 
-type Term struct {
-	tag         string
-	files       []string
-	adjacencies []string
-}
+// convenience shorthand for this awkward type:
+type Set map[string]bool
 
 func ParseQuery(query string) []string {
 	return strings.Split(query, ",")
@@ -86,12 +83,17 @@ func ParseContent(filename string, content *string) Entry {
 	return e
 }
 
-// map tag to filenames
-func MakeTagmap(entries []Entry) map[string][]string {
-	tagmap := make(map[string][]string, len(entries))
+// maps tags to a set of filenames
+func MakeTagmap(entries []Entry) map[string]Set {
+	tagmap := map[string]Set{}
+
 	for _, e := range entries {
 		for _, tag := range e.tags {
-			tagmap[tag] = append(tagmap[tag], e.filename)
+			// allocate submap if necessary:
+			if _, ok := tagmap[tag]; !ok {
+				tagmap[tag] = Set{}
+			}
+			tagmap[tag][e.filename] = true
 		}
 	}
 	return tagmap
@@ -100,10 +102,10 @@ func MakeTagmap(entries []Entry) map[string][]string {
 // adjacencies is a map from tag to other tags occuring in all files.
 //
 // technically a map[tag]set : go's "set" being a map[T]bool.
-func Adjacencies(entries []Entry, tagmap map[string][]string) map[string]map[string]bool {
-	adjacencies := make(map[string]map[string]bool, len(entries))
+func Adjacencies(entries []Entry, tagmap map[string]Set) map[string]Set {
+	adjacencies := make(map[string]Set, len(entries))
 	for tag, _ := range tagmap {
-		adjacencies[tag] = map[string]bool{}
+		adjacencies[tag] = Set{}
 	}
 
 	for _, e := range entries {
@@ -112,6 +114,7 @@ func Adjacencies(entries []Entry, tagmap map[string][]string) map[string]map[str
 			copy(others, e.tags)
 			others = slices.Delete(others, i, i+1)
 			set, ok := adjacencies[tag]
+			// TODO: is this necessary?
 			if ok {
 				for _, t := range others {
 					set[t] = true
@@ -123,15 +126,13 @@ func Adjacencies(entries []Entry, tagmap map[string][]string) map[string]map[str
 	return adjacencies
 }
 
-func Grep(entries []Entry, tagmap map[string][]string, queries []string) map[string][]string {
+func Grep(entries []Entry, tagmap map[string]Set, queries []string) map[string]Set {
 	for _, e := range entries {
 		for _, query := range queries {
 			// TODO: in the presence of multiple query strings, this is an OR.
 			// Should be an AND.
 			if strings.Contains(strings.ToLower(e.content), query) {
-				if !slices.Contains(tagmap[query], e.filename) {
-					tagmap[query] = append(tagmap[query], e.filename)
-				}
+				tagmap[query][e.filename] = true
 			}
 		}
 	}
@@ -139,18 +140,18 @@ func Grep(entries []Entry, tagmap map[string][]string, queries []string) map[str
 }
 
 func Collect(
-	tagmap map[string][]string,
-	adjacencies map[string]map[string]bool,
+	tagmap map[string]Set,
+	adjacencies map[string]Set,
 	queries []string,
-) map[string]map[string]bool {
+) map[string]Set {
 
-	collection := map[string]map[string]bool{}
-	collection["files"] = map[string]bool{}
-	collection["adjacencies"] = map[string]bool{}
+	collection := map[string]Set{}
+	collection["files"] = Set{}
+	collection["adjacencies"] = Set{}
 
 	for _, query := range queries {
-		for _, f := range tagmap[query] {
-			collection["files"][f] = true
+		for file, _ := range tagmap[query] {
+			collection["files"][file] = true
 		}
 	}
 
@@ -162,7 +163,7 @@ func Collect(
 	return collection
 }
 
-func PrintCollection(collection map[string]map[string]bool, queries []string, grep bool) {
+func PrintCollection(collection map[string]Set, queries []string, grep bool) {
 	fmt.Println("[tag]")
 	fmt.Println(queries)
 	fmt.Println()
