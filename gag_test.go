8fd5ac3d750678eec518aae5e392d94e6648a71f
bth <gh@hautogdoad.com>
2025-12-21 12:37:14 -0800

    fix tests

--- gag_test.go
+++ gag_test.go
@@ -10,14 +10,14 @@ import (
 const TEST_PATTERN string = "./testdata/*.md"
 
 func TestParseHeader(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	header := ParseHeader(&entries[0].content)
 	expected := "# 01.foo.md\n: 2024.09.25\n+ sot\n+ foo"
 	assert.Equal(t, expected, header)
 }
 
 func TestEntriesLen(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	expected := 6
 	if len(entries) != expected {
 		t.Errorf("entries should be len == %v, got %v", expected, len(entries))
@@ -25,131 +25,64 @@ func TestEntriesLen(t *testing.T) {
 }
 
 func TestEntries(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	d, _ := time.Parse("2006.01.02", "2024.09.25")
 	expected := Entry{filename: "01.foo.md", date: d, content: "# 01.foo.md\n: 2024.09.25\n+ sot\n+ foo\n\nFoo bar.\n", tags: []string{"sot", "foo"}}
 	assert.Equal(t, expected, entries[0])
 }
 
 func TestTagmap(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	tagmap := Tagmap(entries)
 	expected := Set{"01.foo.md": true, "02.foo.md": true, "03.bar.md": true}
 	assert.Equal(t, expected, tagmap["sot"])
 }
 
 func TestAdjacencies(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
-	adjacencies := Adjacencies(entries)
+	entries := Entries(Filelist(TEST_PATTERN))
+	tagmap := Tagmap(entries)
+	queries := ParseQuery("sot")
+	fs := IntersectQueries(tagmap, queries)
+	adjacencies := Adjacencies(entries, fs)
 	expected := Set{"science": true, "foo": true}
 	assert.Equal(t, expected, adjacencies["sot"])
 }
 
-func TestGrep(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("foo")
-	tagmap := Tagmap(entries)
-
-	tagmap = Grep(entries, tagmap, queries)
-	expected := Set{"01.foo.md": true, "02.foo.md": true, "04.baz.md": true}
-	assert.Equal(t, expected, tagmap["foo"])
-}
-
 func TestBadTag(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("qaz")
+	entries := Entries(Filelist(TEST_PATTERN))
 	tagmap := Tagmap(entries)
 
-	// test that these won't panic
-	tagmap = Grep(entries, tagmap, queries)
-	tagmap = Find(entries, tagmap, queries)
-	tagmap = Diff(entries, tagmap, queries)
 	_, ok := tagmap["qaz"]
 	assert.False(t, ok)
 }
 
-func TestFind(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("baz")
-	tagmap := Tagmap(entries)
-
-	tagmap = Find(entries, tagmap, queries)
-	expected := Set{"04.baz.md": true}
-	assert.Equal(t, expected, tagmap["baz"])
-}
-
-func TestDiff(t *testing.T) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("diff")
-	tagmap := Tagmap(entries)
-	tagmap = Grep(entries, tagmap, queries)
-
-	tagmap = Diff(entries, tagmap, queries)
-	expected := Set{"06.quz.md": true}
-	assert.Equal(t, expected, tagmap["diff"])
-}
-
 // Since Entries() involves filesystem reads, we test the underlying logic.
 func BenchmarkParseContent(b *testing.B) {
-	e := Entries(TEST_PATTERN)[0]
+	e := Entries(Filelist(TEST_PATTERN))[0]
 	for b.Loop() {
 		ParseContent(e.filename, &e.content)
 	}
 }
 
 func BenchmarkTagmap(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	for b.Loop() {
 		Tagmap(entries)
 	}
 }
 
 func BenchmarkAdjacencies(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
-	for b.Loop() {
-		Adjacencies(entries)
-	}
-}
-
-func BenchmarkGrep(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("foo")
+	entries := Entries(Filelist(TEST_PATTERN))
 	tagmap := Tagmap(entries)
-	for b.Loop() {
-		Grep(entries, tagmap, queries)
-	}
-}
-
-func BenchmarkFind(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
 	queries := ParseQuery("foo")
-	tagmap := Tagmap(entries)
+	fs := IntersectQueries(tagmap, queries)
 	for b.Loop() {
-		Find(entries, tagmap, queries)
-	}
-}
-
-func BenchmarkDiff(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("foo")
-	tagmap := Tagmap(entries)
-	for b.Loop() {
-		Diff(entries, tagmap, queries)
-	}
-}
-
-func BenchmarkCollect(b *testing.B) {
-	entries := Entries(TEST_PATTERN)
-	queries := ParseQuery("foo")
-	tagmap := Tagmap(entries)
-	adjacencies := Adjacencies(entries)
-	for b.Loop() {
-		Collect(tagmap, adjacencies, queries)
+		Adjacencies(entries, fs)
 	}
 }
 
 func FuzzParseContent(f *testing.F) {
-	entries := Entries(TEST_PATTERN)
+	entries := Entries(Filelist(TEST_PATTERN))
 	for _, e := range entries {
 		f.Add(e.filename, e.content)
 	}
